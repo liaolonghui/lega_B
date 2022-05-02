859ae0959c11d749111b8f3ec9fb8b4d71ce1312
liaolonghui <1340881930@qq.com>
2022-05-02 12:56:05 +0800

    写完UserProfile的测试

--- tests/unit/UserProfile.spec.ts
+++ tests/unit/UserProfile.spec.ts
@@ -1,10 +1,21 @@
 import { mount, VueWrapper } from "@vue/test-utils";
 import UserProfile from "@/components/UserProfile.vue";
+import { message } from "ant-design-vue";
+import store from "@/store/index"
 
 let wrapper: VueWrapper<any>
-jest.mock('ant-design-vue')
-jest.mock('vuex')
-jest.mock('vue-router')
+jest.mock('ant-design-vue', () => ({
+  message: {
+    success: jest.fn()
+  }
+}))
+// jest.mock('vuex')
+const mockRoutes: string[] = []
+jest.mock('vue-router', () => ({
+  useRouter: () => ({
+    push: (url: string) => mockRoutes.push(url)
+  })
+}))
 
 const mockComponent1 = {
   template: '<div><slot></slot></div>'
@@ -22,18 +33,26 @@ const mockComponents = {
 
 describe('test UserProfile component', () => {
   beforeAll(() => {
+    jest.useFakeTimers()
     wrapper = mount(UserProfile, {
       props: {
         user: { isLogin: false }
       },
       global: {
-        components: mockComponents
+        components: mockComponents,
+        provide: {
+          store
+        }
       }
     })
   })
-  it('UserProfile when isLogin is false', () => {
+  it('UserProfile when isLogin is false', async () => {
     // console.log(wrapper.html())
     expect(wrapper.get('div').text()).toBe('登录')
+    // 点击登录
+    await wrapper.get('div').trigger('click')
+    expect(message.success).toHaveBeenCalled()
+    expect(store.state.user.userName).toBe('廖龙辉')
   })
   it('UserProfile when islogin is true', async () => {
     await wrapper.setProps({
@@ -43,8 +62,17 @@ describe('test UserProfile component', () => {
     expect(wrapper.get('div.user-profile-dropdown > div').text()).toBe('登出')
     expect(wrapper.get('.user-profile-component').html()).toContain('廖龙辉')
     expect(wrapper.find('.user-profile-component').exists()).toBeTruthy()
+    // 点击登出
+    await wrapper.get('div.user-profile-dropdown > div').trigger('click')
+    expect(store.state.user.isLogin).toBeFalsy()
+    expect(message.success).toBeCalledTimes(1)
+    jest.runAllTimers() // 跑完所有timer
+    expect(mockRoutes).toEqual(['/'])
   })
   afterAll(() => {
     // config.renderStubDefaultSlot = false
+  })
+  afterEach(() => {
+    (message as jest.Mocked<typeof message>).success.mockReset()
   })
 })
